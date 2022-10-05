9bec7a91179aa53c127d2b0a432e8072832de721
Sunit <sunit.corneleous25@gmail.com>
2022-10-05 12:11:11 +0600

    rating badge updated for movies

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -11,7 +11,9 @@ const Show = ({ show }) => {
       <div className="card-body">
         <h2 className="card-title">
           {show.title}
-          <div className="badge badge-secondary">{show.vote_average}</div>
+          <div className="w-10 text-center rounded-2xl text-lg border-solid border-2 border-white bg-orange-600 text-white">
+            {show.vote_average}
+          </div>
         </h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
       </div>
